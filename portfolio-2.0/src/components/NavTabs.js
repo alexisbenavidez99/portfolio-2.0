198fe323c7f38620f72fa5c9f67bdd24c32de1a4
alexisbenavidez99 <callmealexis49@gmail.com>
2023-03-11 14:28:08 -0600

    adding changes to navbar and about page

--- portfolio-2.0/src/components/NavTabs.js
+++ portfolio-2.0/src/components/NavTabs.js
@@ -2,39 +2,45 @@ import React from 'react';
 
 export default function NavTabs({ currentPage, handlePageChange }) {
     return (
-        <ul className="nav justify-content-end">
-            <li className="nav-item">
-                <a
-                    className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                    href="#about-me"
-                    onClick={() => handlePageChange('About')}
-                >About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-                    href="#projects"
-                    onClick={() => handlePageChange('Projects')}
-                >Projects
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                    href="#contact"
-                    onClick={() => handlePageChange('Contact')}
-                >Contact Me
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                    href="#resume"
-                    onClick={() => handlePageChange('Resume')}
-                >Resume
-                </a>
-            </li>
-        </ul>
+        <div>
+            <ul className="nav">
+            <div className="nav-header">
+            <h1>Alexis</h1>
+            <h1>Benavidez.</h1>
+            </div>
+                <li className="nav-item">
+                    <a
+                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                        href="#about-me"
+                        onClick={() => handlePageChange('About')}
+                    >About
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                        href="#projects"
+                        onClick={() => handlePageChange('Projects')}
+                    >Projects
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                        href="#contact"
+                        onClick={() => handlePageChange('Contact')}
+                    >Contact Me
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                        href="#resume"
+                        onClick={() => handlePageChange('Resume')}
+                    >Resume
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }

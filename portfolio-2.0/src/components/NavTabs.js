45eb5062578a374eb1664f2fca62d689cdf8dfb2
alexisbenavidez99 <callmealexis49@gmail.com>
2023-03-11 13:31:10 -0600

    added starter about section

--- portfolio-2.0/src/components/NavTabs.js
+++ portfolio-2.0/src/components/NavTabs.js
@@ -11,7 +11,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                 >About
                 </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <a
                     className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
                     href="#projects"
@@ -19,7 +19,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                 >Projects
                 </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <a
                     className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                     href="#contact"
@@ -27,7 +27,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                 >Contact Me
                 </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <a
                     className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
                     href="#resume"
---
+++ portfolio-2.0/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>Hi,<br>
+            My name is Alexis Benavidez.</br></h1>
+            <p>
+            I am an aspiring web developer. I just obtained a certficate for Full-Stack Web Development. My area of interests are front-end technologies such as HTML, CSS, React, JavaScript.
+            I am always eager to learn different things and expand my knowledge anyway I can! My portfolio showcases different projects I've made and collaborated on.
+            Take a look around!
+            </p>
+        </div>
+    )
+}

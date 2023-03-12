5c21b9a17d7da941353ce396bfaa9d1b94674b96
alexisbenavidez99 <callmealexis49@gmail.com>
2023-03-12 15:31:11 -0500

    changed design of navbar and about section

--- portfolio-2.0/src/components/NavTabs.js
+++ portfolio-2.0/src/components/NavTabs.js
@@ -3,44 +3,48 @@ import React from 'react';
 export default function NavTabs({ currentPage, handlePageChange }) {
     return (
         <div>
-            <ul className="nav">
-            <div className="nav-header">
-            <h1>Alexis</h1>
-            <h1>Benavidez.</h1>
-            </div>
-                <li className="nav-item">
-                    <a
-                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                        href="#about-me"
-                        onClick={() => handlePageChange('About')}
-                    >About
+            <nav className="navbar bg-body">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#about-me">
+                        <p className="name-icon">Alexis</p>
+                        <p className="name-icon">Benavidez.</p>
                     </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-                        href="#projects"
-                        onClick={() => handlePageChange('Projects')}
-                    >Projects
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                        href="#contact"
-                        onClick={() => handlePageChange('Contact')}
-                    >Contact Me
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                        href="#resume"
-                        onClick={() => handlePageChange('Resume')}
-                    >Resume
-                    </a>
-                </li>
-            </ul>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item">
+                            <a
+                                className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+                                href="#about-me"
+                                onClick={() => handlePageChange('About Me')}
+                            >About Me
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                                href="#projects"
+                                onClick={() => handlePageChange('Projects')}
+                            >Projects
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                                href="#contact"
+                                onClick={() => handlePageChange('Contact')}
+                            >Contact Me
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                                href="#resume"
+                                onClick={() => handlePageChange('Resume')}
+                            >Resume
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
--- portfolio-2.0/src/components/pages/About.js
+++ portfolio-2.0/src/components/pages/About.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div>
+        <div className="about">
             <h1 className="about-header">Hi,</h1>
             <h1 className="about-header">My name is Alexis Benavidez.</h1>
-            <p>
-            I am an aspiring web developer. I just obtained a certficate for Full-Stack Web Development. My area of interests are front-end technologies such as HTML, CSS, React, JavaScript.
-            I am always eager to learn different things and expand my knowledge anyway I can! My portfolio showcases different projects I've made and collaborated on.
-            Take a look around!
+            <p className="about-section">
+                I am an aspiring web developer. I just obtained a certficate for Full-Stack Web Development. My area of interests are front-end technologies such as HTML, CSS, React, JavaScript.
+                I am always eager to learn different things and expand my knowledge anyway I can! My portfolio showcases different projects I've made and collaborated on.
+                Take a look around!
             </p>
         </div>
     )

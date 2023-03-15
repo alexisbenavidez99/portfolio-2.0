b4da1312e270c84a18800efd7c989d1161743e50
alexisbenavidez99 <callmealexis49@gmail.com>
2023-03-14 20:12:41 -0500

    fixed footer to bottom of page

--- portfolio-2.0/src/components/Footer.js
+++ portfolio-2.0/src/components/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className='footer'>
-            <footer className="text-center text-lg-start grid-gap-3">
+            <footer className="text-center text-lg-start grid-gap-3 fixed-bottom">
                 <div className="text-center p-3">
-                        <a className="text-dark p-2 g-col-2" href="https://github.com/alexisbenavidez99">GH</a>
-                        <a className="text-dark p-2 g-col-2" href="https://www.linkedin.com/in/alexis-benavidez-b0b768265/">LI</a>
-                        <a className="text-dark p-2 g-col-2" href="https://github.com/alexisbenavidez99">EM</a>
+                        <a className="p-2 g-col-2" href="https://github.com/alexisbenavidez99">GH</a>
+                        <a className="p-2 g-col-2" href="https://www.linkedin.com/in/alexis-benavidez-b0b768265/">LI</a>
+                        <a className="p-2 g-col-2" href="https://github.com/alexisbenavidez99">EM</a>
                 </div>
             </footer>
         </div>
